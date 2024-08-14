42e29ed700c14961bd619b755295fb4dd0798f9c
BhuwanRajAwasthi <bhuwanraj123456@gmail.com>
2024-08-14 09:34:20 +0545

    added reacthookform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import CompanyProfile from "./components/Project-main/Company_profile";
 import Jobcard from "./components/Project-main/Jobcard";
+import { ProfileForm } from "./reacthookform";
 import CompanyProfile2 from "./components/Project-main/Company_profile2";
 import "./styles.css";
 import Userprofile from "./components/Project-main/Userprofile";
@@ -11,6 +12,7 @@ export default function Home() {
       <Userprofile Name={"Bhuwan Raj Awasthi"} Age={0} Main_Profession={""} Email={"bhuwanraj123456@gmail.com"} Phone={0} Address={"bagar-1,pokhara"} Bio={"Hello my name is bhuwan raj awasthi. I am Currently studying at prithivhi narayan campus it's cool to study here,as i wanted beautiful, with greeny and fresh environment where you meet different peoples "} Image={"./gentlemen.jpg"} LinkedIn={"https://LinkedIn/BhuwanRajAwasthi"} GitHub={""} Skills={["Ux designer", "Java", "Kotlin"]} Experience={0} Education={""}excompany_name={["The misfit of night raid"]} Certifications={[]} Job_Experience={[]} experience_company={["Product Designer"]}/>
       <CompanyProfile />
       <CompanyProfile2/>
+      <ProfileForm/>
     </main>
   );
 }
